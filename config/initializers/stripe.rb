b5d430b63b49b59730efae24d022f9a504e4efa9
Ahmed-Nazeeh <aa.nazeeh@gmail.com>
2022-08-12 10:01:27 +0200

    fix bug - configure stripe for production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,9 @@
-Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+Rails.configuration.stripe = {
+    :secret_key => ENV['STRIPE_SECRET_KEY']
+}
+
+if Rails.env.production?
+    Stripe.api_key = Rails.configuration.stripe
+else 
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
+end
