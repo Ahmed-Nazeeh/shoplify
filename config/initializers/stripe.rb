b147d25ecc1ecd0af87dfe9bee7e84bb620d6051
Ahmed-Nazeeh <aa.nazeeh@gmail.com>
2022-08-12 10:26:02 +0200

    fix stripe heroku bugs 2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,10 @@
 Rails.configuration.stripe = {
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
     :secret_key => ENV['STRIPE_SECRET_KEY']
 }
 
 if Rails.env.production?
-    Stripe.api_key = Rails.configuration.stripe
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
 else 
     Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
 end
